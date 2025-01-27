98ad29007bd86215eb0a991cbc00a5bb8b1ef814
Neyrijus <alfcf@discente.ifpe.edu.br>
2025-01-26 23:11:12 -0300

    Add files via upload

--- src/JS/index.js
+++ src/JS/index.js
@@ -15,37 +15,41 @@ anterior da lista
     - passo 4 - buscar o cartão que esta selecionado e esconder
 */
 
-
-
 const btnAvancar = document.getElementById("btn-avancar");
 const btnVoltar = document.getElementById("btn-voltar");
 const cartoes = document.querySelectorAll(".cartao");
 let cartaoAtual = 0;
 
-btnAvancar.addEventListener("click", function () {
-    console.log(cartaoAtual);
-    console.log(cartoes.length - 1)
+cartoes.forEach(cartao => {
+    cartao.addEventListener("click", function () {
+        const cartaVirada = cartao.querySelector(".carta-virada");
 
+        // virar o cartão
+        cartao.classList.toggle("virar");
+        // mostrar o fundo da carta
+        cartaVirada.classList.toggle("mostrar-fundo-carta");
+
+        const descricao = cartao.querySelector(".descricao");
+        descricao.classList.toggle("esconder");
+    });
+});
+
+btnAvancar.addEventListener("click", function () {
     if (cartaoAtual === cartoes.length - 1) return;
 
     esconderCartaoSelecionado();
 
     cartaoAtual++;
     mostrarCartao(cartaoAtual);
-
 });
 
 btnVoltar.addEventListener("click", function () {
-    console.log(cartaoAtual);
-    console.log(cartoes.length - 1)
-
     if (cartaoAtual === 0) return;
 
     esconderCartaoSelecionado();
 
     cartaoAtual--;
     mostrarCartao(cartaoAtual);
-
 });
 
 function mostrarCartao(cartaoAtual) {
@@ -56,3 +60,5 @@ function esconderCartaoSelecionado() {
     const cartaoSelecionado = document.querySelector(".selecionado");
     cartaoSelecionado.classList.remove("selecionado");
 }
+
+
